4be943d66c961013ce9e066df0ca8478a6506092
rezaulislam <mrezaulislam.t@gmail.com>
2024-10-19 23:48:56 +0600

    Initial Commit

---
+++ AnyBlog/AnyBlogApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnyBlogApp.swift
+//  AnyBlog
+//
+//  Created by Rezaul Islam on 18/10/24.
+//
+
+import SwiftUI
+import Blog
+
+@main
+struct AnyBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BlogPostScreen()
+        }
+    }
+}
---
+++ AnyBlog/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AnyBlog
+//
+//  Created by Rezaul Islam on 18/10/24.
+//
+
+import SwiftUI
+import Blog
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
